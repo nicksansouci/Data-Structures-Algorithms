63c4548118aabf3b8a663088af786399467b0636
nicksansouci@gmail.com <nicksansouci@gmail.com>
2022-03-06 12:37:40 -0700

    Updated remove vertex function

--- graph.py
+++ graph.py
@@ -22,10 +22,22 @@ class Graph:
 
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            self.adj_list[v1].remove(v2)
-            self.adj_list[v2].remove(v1)
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
             return True
         return False
+
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
 
 my_graph = Graph()
 
@@ -36,6 +48,7 @@ my_graph.add_edge('A', 'B')
 my_graph.add_edge('B', 'C')
 my_graph.add_edge('C', 'A')
 my_graph.remove_edge('A', 'B')
+my_graph.remove_vertex('C')
 
 
 my_graph.print_graph()
