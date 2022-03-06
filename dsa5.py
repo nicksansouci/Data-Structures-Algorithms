58e1a4c613066eadce1b38ec46542bc01d590875
nicksansouci@gmail.com <nicksansouci@gmail.com>
2022-03-06 12:17:38 -0700

    Added Graph

--- dsa5.py
+++ dsa5.py
@@ -11,7 +11,31 @@ class HashTable:
         for i, val in enumerate(self.data_map):
             print(i, ":", val)
 
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
 
 my_table = HashTable()
+my_table.set_item('Nick', 69)
+my_table.set_item('Johnny', 70)
 
+print(my_table.keys())
 my_table.print_table()
---
+++ graph.py
@@ -0,0 +1,41 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        else:
+            return False
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex ,":", self.adj_list[vertex])
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+            return True
+        return False
+
+my_graph = Graph()
+
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('B', 'C')
+my_graph.add_edge('C', 'A')
+my_graph.remove_edge('A', 'B')
+
+
+my_graph.print_graph()
