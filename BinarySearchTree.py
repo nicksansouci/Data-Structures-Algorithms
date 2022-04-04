afe2c2b6c76b433ad6a16dc6b7cc130f78ca8462
nicksansouci@gmail.com <nicksansouci@gmail.com>
2022-04-04 09:39:09 -0700

    Added Breadth First Search for Tree Traversal

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -47,14 +47,30 @@ class BinarySearchTree:
         while current_node.left is not None:
             current_node = current_node.left
         return current_node
-                    
+
+    def BFS(self):
+        current_node= self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+        return results
+
         
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
-print(my_tree.contains(4))
-print(my_tree.min_val(my_tree.root))
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.BFS())
