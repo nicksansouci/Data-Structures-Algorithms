ae2d879df1aa8d0fa08c66606aa1785936f9a338
nicksansouci@gmail.com <nicksansouci@gmail.com>
2022-04-04 09:53:12 -0700

    Added DFS Pre Order

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -63,6 +63,24 @@ class BinarySearchTree:
 
         return results
 
+
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+        
+        
+        
+        
+        
+        
+        
         
 my_tree = BinarySearchTree()
 my_tree.insert(47)
@@ -74,3 +92,5 @@ my_tree.insert(52)
 my_tree.insert(82)
 
 print(my_tree.BFS())
+print(my_tree.dfs_pre_order())
+
